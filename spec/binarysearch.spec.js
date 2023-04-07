fba0894d028cde57bfb1862a591df3b0e489a3a2
Svetlana Govyazina <govyazina@gmail.com>
2023-04-07 16:43:46 +0300

    tests added

--- spec/binarysearch.spec.js
+++ spec/binarysearch.spec.js
@@ -1,13 +1,15 @@
 const binarySearch = require('../index')
 
 test('Функция должна возвращать индекс элемента', () => {
-    
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)).toBe(4)
+    expect(binarySearch(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], 'G')).toBe(6)
 })
 
 test('Функция должна быть определена', () => {
-    
+    expect(binarySearch).toBeDefined()
 })
 
 test('Функция должна возвращать -1, если элемента не существует в данном массиве', () => {
-    
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11)).toBe(-1)
+    expect(binarySearch(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], 1)).toBe(-1)
 })
