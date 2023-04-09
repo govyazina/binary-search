dfe6c1c1f2a22e15160d5d8225adfe4c3d32c015
Svetlana Govyazina <govyazina@gmail.com>
2023-04-09 19:31:39 +0300

    function refactored

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 // Этот метод должен искать элемент в массиве и возвращать его индекс или -1, если элемент не находится в массиве.
 // Например, если ты ищешь G в массиве [A, B, C, D, E, F, G, H], твой метод должен возвращать 6. Если ты ищешь X, он должен возвращать -1.
 
-function binarySearch(arr, elem, start, end) {
-    let indexStart = start === undefined ? 0 : start
-    let indexEnd = end === undefined ? arr.length - 1 : end
-    let i = Math.ceil((indexEnd + indexStart) / 2)
-    if (arr[i] === elem) {
-        return i
+function binarySearch(arr, elem) {
+    const searchRec = (arr, elem, start, end) => {
+        const i = Math.ceil((end + start) / 2)
+        if (arr[i] === elem) {
+            return i
+        }
+        if (start === end) {
+            return -1
+        }
+        if (arr[i] < elem) {
+            start = i + 1
+        }
+        if (arr[i] > elem) {
+            end = i - 1
+        }
+        return searchRec(arr, elem, start, end)
     }
-    if (indexStart === indexEnd) {
-        return -1
-    }
-    if (arr[i] < elem) {
-        indexStart = i + 1
-    }
-    if (arr[i] > elem) {
-        indexEnd = i - 1
-    }
-    return binarySearch(arr, elem, indexStart, indexEnd)
+    return searchRec(arr, elem, 0, arr.length - 1)
 }
 
 module.exports = binarySearch
-
-/*
-[1 3 5]
-o, 2
-1
-[1]
-
-
- */
