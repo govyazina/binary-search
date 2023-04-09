7b73d9e76f5af019d6379e42f6c6d8b4cb826e74
Svetlana Govyazina <govyazina@gmail.com>
2023-04-09 19:00:17 +0300

    function added, tests changed

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 // Этот метод должен искать элемент в массиве и возвращать его индекс или -1, если элемент не находится в массиве.
 // Например, если ты ищешь G в массиве [A, B, C, D, E, F, G, H], твой метод должен возвращать 6. Если ты ищешь X, он должен возвращать -1.
 
-function binarySearch (){
-
+function binarySearch(arr, elem, start, end) {
+    let indexStart = start === undefined ? 0 : start
+    let indexEnd = end === undefined ? arr.length - 1 : end
+    let i = Math.ceil((indexEnd + indexStart) / 2)
+    if (arr[i] === elem) {
+        return i
+    }
+    if (indexStart === indexEnd) {
+        return -1
+    }
+    if (arr[i] < elem) {
+        indexStart = i + 1
+    }
+    if (arr[i] > elem) {
+        indexEnd = i - 1
+    }
+    return binarySearch(arr, elem, indexStart, indexEnd)
 }
 
 module.exports = binarySearch
+
+/*
+[1 3 5]
+o, 2
+1
+[1]
+
+
+ */
--- spec/binarysearch.spec.js
+++ spec/binarysearch.spec.js
@@ -1,15 +1,20 @@
 const binarySearch = require('../index')
 
-test('Функция должна возвращать индекс элемента', () => {
-    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)).toBe(4)
-    expect(binarySearch(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], 'G')).toBe(6)
-})
+describe('binary search', function () {
+    test('Функция должна возвращать индекс элемента', () => {
+        expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)).toBe(4)
+        expect(binarySearch(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], 'G')).toBe(6)
+    })
 
-test('Функция должна быть определена', () => {
-    expect(binarySearch).toBeDefined()
-})
+    test('Функция должна быть определена', () => {
+        expect(binarySearch).toBeDefined()
+    })
 
-test('Функция должна возвращать -1, если элемента не существует в данном массиве', () => {
-    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11)).toBe(-1)
-    expect(binarySearch(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], 1)).toBe(-1)
-})
+    test('Функция должна возвращать -1, если элемента не существует в данном массиве', () => {
+
+        expect(binarySearch([1,  3,  5], 2)).toBe(-1)
+        expect(binarySearch([1,  3, 4, 5], 2)).toBe(-1)
+        expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11)).toBe(-1)
+        expect(binarySearch(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], 'X')).toBe(-1)
+    })
+});
