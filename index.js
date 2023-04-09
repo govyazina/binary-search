b5600823aebb7ee6a9f988e10b089c455d7df026
Svetlana Govyazina <govyazina@gmail.com>
2023-04-09 19:36:10 +0300

    function refactored (senior level)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Например, если ты ищешь G в массиве [A, B, C, D, E, F, G, H], твой метод должен возвращать 6. Если ты ищешь X, он должен возвращать -1.
 
 function binarySearch(arr, elem) {
-    const searchRec = (arr, elem, start, end) => {
+    const searchRec = (start, end) => {
         const i = Math.ceil((end + start) / 2)
         if (arr[i] === elem) {
             return i
@@ -10,15 +10,11 @@ function binarySearch(arr, elem) {
         if (start === end) {
             return -1
         }
-        if (arr[i] < elem) {
-            start = i + 1
-        }
-        if (arr[i] > elem) {
-            end = i - 1
-        }
-        return searchRec(arr, elem, start, end)
+        return arr[i] < elem
+            ? searchRec(i + 1, end)
+            : searchRec(start, i - 1)
     }
-    return searchRec(arr, elem, 0, arr.length - 1)
+    return searchRec(0, arr.length - 1)
 }
 
 module.exports = binarySearch
